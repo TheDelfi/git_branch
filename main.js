2a81ee53bc154251ca65deb1771baf1b66354f78
TheDelfi <demidpro5@gmail.com>
2025-05-20 03:02:07 +0300

    class people added

--- main.js
+++ main.js
@@ -21,3 +21,16 @@ function loll(){
     console.log('swag')
 
 }
+
+class people{
+    constructor(name,age,status){
+        this.name = name;
+        this.age = age;
+        this.status = status
+    }
+}
+
+
+const demid = new people('Demid',15,'free')
+
+console.log(demid.name)
