e1c833aa0691a61359864b0c2c998334cd502fab
TheDelfi <demidpro5@gmail.com>
2025-05-21 18:27:01 +0300

    remove file oge

--- 16.js
+++ 16.js
@@ -1,10 +1,17 @@
-const nums = [1,2,3,4,5,6,7,8,12]
-let result_number = 0
+const nums = [22,11,2,3]
+let summa_num = 0
+let skolko_chisel = 0
+let result_num = 0
 
-for (let i of nums){
-    if (i % 3 == 0 &&  i % 10 == 2){
-        result_number += i
+
+
+for(i of nums){
+    if(String(i).length == 2){
+        skolko_chisel += 1
+        summa_num += i
     }
 }
 
-console.log(result_number)
+
+result_num = summa_num / skolko_chisel
+console.log(result_num)
